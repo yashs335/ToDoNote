97d463ac3ff76c8bd1ee47bdca7e1c80af30cc38
yashs335 <yashs@webmobtech.com>
2025-05-12 14:11:22 +0530

    adding navigation and new activity for the add task

--- app/src/main/java/com/example/todonote/MainActivity.kt
+++ app/src/main/java/com/example/todonote/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.example.todonote.model.TaskModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +17,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-//        val intent : Intent = Intent()
 //
 //        startActivity(intent.apply {  })
 
-//        val button : Button = findViewById(R.id.navigate_add_task)
-//        button.setOnClickListener{
-//            navigateToAddNewTask()
-//        }
+        val button : Button = findViewById(R.id.navigate_add_task)
+        button.setOnClickListener{
+            val intent : Intent = Intent(this,AddNewTaskActivity::class.java)
+            startActivity(intent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
---
+++ app/src/main/java/com/example/todonote/handler/DatabaseHandler.kt
@@ -0,0 +1,4 @@
+package com.example.todonote.handler
+
+class DatabaseHandler {
+}

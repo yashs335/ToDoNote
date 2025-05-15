dc35825527a9ce5db5691af19eab639527455fa7
yashs335 <yashs@webmobtech.com>
2025-05-15 10:01:14 +0530

    create validation for the login and signup screen

--- app/src/main/java/com/example/todonote/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/todonote/fragment/LoginFragment.kt
@@ -2,13 +2,17 @@ package com.example.todonote.fragment
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.todonote.R
+import com.example.todonote.validation.validation
 import com.example.todonote.view.Communicator
+import org.intellij.lang.annotations.Pattern
 
 class LoginFragment : Fragment(R.layout.sign_in_screen) {
 
@@ -27,12 +31,26 @@ class LoginFragment : Fragment(R.layout.sign_in_screen) {
         val email = view.findViewById<EditText>(R.id.edit_text_email_login)
         val pass = view.findViewById<EditText>(R.id.edit_text_password_login)
 
+
         view.findViewById<TextView?>(R.id.sign_up_nav_button)?.setOnClickListener {
             communicator.setFragment(SignUpScreen())
         }
         view.findViewById<Button>(R.id.login_button)?.setOnClickListener{
-            communicator.getLogin(email.text.toString(),pass.text.toString())
+            if(email.text.trim().isEmpty() || pass.text.trim().isEmpty()){
+                email.error = "Please enter the email"
+                pass.error = "please enter the pass"
+            }
+            else if(!Patterns.EMAIL_ADDRESS.matcher( email.text.toString()).matches()) {
+                email.error = "Please Enter Valid Address"
+            }else if(pass.text.length < 8){
+                pass.error = "Please Enter minimum 8 character"
+            } else{
+//                Toast.makeText(context,"login checked",Toast.LENGTH_LONG).show()
+                communicator.getLogin(email.text.toString(),pass.text.toString())
+            }
         }
+
+
     }
 
 }
--- app/src/main/java/com/example/todonote/fragment/SignUpScreen.kt
+++ app/src/main/java/com/example/todonote/fragment/SignUpScreen.kt
@@ -3,10 +3,12 @@ package com.example.todonote.fragment
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.todonote.R
 import com.example.todonote.presenter.Presenter
@@ -35,7 +37,19 @@ class SignUpScreen() : Fragment(R.layout.sign_up_screen) {
         }
 
         view.findViewById<Button>(R.id.sign_up_button)?.setOnClickListener{
-            communicator.getSignUp(email.text.toString(),pass.text.toString(),userName.text.toString())
+            if(email.text.trim().isEmpty() || pass.text.trim().isEmpty() || userName.text.trim().isEmpty()){
+                email.error = "Please enter the email"
+                pass.error = "please enter the pass"
+                userName.error = "Please enter the user_name"
+            }
+            else if(!Patterns.EMAIL_ADDRESS.matcher( email.text.toString()).matches()) {
+                email.error = "Please Enter Valid Address"
+            }else if(pass.text.length < 8){
+                pass.error = "Please Enter minimum 8 character"
+            } else{
+//                Toast.makeText(context,"login checked",Toast.LENGTH_LONG).show()
+                communicator.getSignUp(email.text.toString(),pass.text.toString(),userName.text.toString())
+            }
         }
 
 
